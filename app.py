11287cd7f6955615cd28eeb3bb1c078e7345576e
cyan-mv <cyan.mv@gmail.com>
2023-09-25 03:40:34 -0600

    Lowercase json :)

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ import subprocess
 
 app = Flask(__name__)
 
-@app.route('/')
+""" @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('result.html') """
 
-@app.route('/run_script', methods=['POST'])
-def run_script():
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    fields = ["comunidad", "primer nivel", "segundo nivel", "centros"]
+    #if request.method == 'POST': 
+    user_input = request.form.get('field1')
+    result = user_input
+    return render_template('index.html', result=result)
+
+
+@app.route('/generate_excel', methods=['POST'])
+def generate_excel():
     try:
         # Run the script using subprocess
         subprocess.run(['python', 'generate_excel.py'], check=True)
@@ -16,21 +26,6 @@ def run_script():
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
-""" @app.route('/process_form', methods=['POST'])
-def process_form():
-    field1 = request.form.get('field1')
-    field2 = request.form.get('field2')
-    field3 = request.form.get('field3')
-    field4 = request.form.get('field4')
-
-    # Create a list with the values
-    input_values = [field1, field2, field3, field4]
-
-    # Do something with the input_values list
-    # ...
-
-    return render_template('result.html', input_values=input_values) """
-    
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
